7e85700195ca3d28c033f96ababf5c3cb5e6355f
Julien <julienawilson@gmail.com>
2017-02-25 14:52:45 -0800

    looping celery

--- learncelery.py
+++ learncelery.py
@@ -1,4 +1,5 @@
 from celery import Celery
+from tasks import add
 
 app = Celery('learncelery', broker='redis://localhost:6379/0')
 
@@ -6,3 +7,11 @@ app = Celery('learncelery', broker='redis://localhost:6379/0')
 @app.task
 def hello():
     return 'hello world'
+
+
+def repeat_add(num):
+    """Repeat the add function a few times."""
+    result = []
+    for i in range(num):
+        result.append(add.delay(i, i))
+    return result
--- tasks.py
+++ tasks.py
@@ -5,6 +5,15 @@ app = Celery('tasks', broker='amqp://localhost//')
 
 app.conf.result_backend = 'redis://'
 
+
 @app.task
 def add(x, y):
     return x + y
+
+
+def repeat_add(num):
+    """Repeat the add function a few times."""
+    result = []
+    for i in range(num):
+        result.append(add.delay(i, i))
+    return result
